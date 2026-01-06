eddd83f0857953ac7ca6f2676667318a24d0bb51
Luan Sampaio <luansampaio0294@gmail.com>
2026-01-06 10:59:35 -0300

    feat(cli): add initial interface

--- src/main.py
+++ src/main.py
@@ -1,1 +1,36 @@
-print("Hello World!")
+import os
+
+def main():
+    start_screen()
+    get_input = input()
+    insert_book_screen()
+
+
+def start_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("               LIBMANAGER v1.0")
+    print("    =====================================")
+    print()    
+    print("         Escolha uma opção:")
+    print()
+    print("         [1] Cadastrar livro")
+    print("         [2] Editar livro")
+    print("         [3] Excluir livro")
+    print("         [4] Visualizar livros")
+    print("         [5] Sair do programa")
+    print()
+    print("    =====================================")
+    print("    Digite sua opção abaixo: ")
+    print("    > _", end="")
+
+
+def insert_book_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("    ============= CADASTRO =============")
+    print()
+    print("             Preencha os dados abaixo.")
+    print()
+    print("    =====================================")
+
+
+main()
