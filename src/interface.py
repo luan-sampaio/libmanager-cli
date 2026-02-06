cb4bac7ed4638b1744326a30802c1e734b5dfcc7
Luan Sampaio <luansampaio0294@gmail.com>
2026-02-06 10:44:17 -0300

    Revert "fix: reverse the orde of book's attributes"
    This reverts commit d83ddc7721d8d538267381c22ad6367228366cbb.

--- src/interface.py
+++ src/interface.py
@@ -154,7 +154,7 @@ def get_book():
             break
 
     book_id = utils.increase_id()
-    return {"id": book_id, "title": title, "author": author, "date": date}
+    return {"title": title, "author": author, "date": date, "id": book_id}
 
 
 def show_list_books(list_books):
